e9f830966dc53f67393dd93287c1c05024f0cfaf
tevin davis <aod.td1105@gmail.com>
2020-04-26 15:54:23 -0400

    Added update, post and delete operations to accountRouter file

---
+++ account/accountRouter.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const db = require('../data/dbConfig');
+
+const router = express.Router()
+
+router.get('/', async (req, res, next) => {
+    try {
+        const accounts = await db.select("*").from("accounts")
+        console.log(accounts)
+        res.json(accounts)
+    }
+    catch(err) {
+        next(err)
+    }
+})
+
+router.get('/:id', async (req, res, next) => {
+    try {
+        const account = 
+        await db.select("*").from("accounts").where("id", req.params.id).limit(1)
+
+        res.json(account)
+    }
+    catch(err) {
+        next(err)
+    }
+})
+
+router.post('/', async (req, res, next) => {
+    try {
+        const payload = {
+            name: req.body.name,
+            budget: req.body.budget
+        }
+
+        const [id] = await db("accounts").insert(payload)
+        const account = await db("accounts").where("id", id).first()
+
+        res.json(message)
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.put('/:id', async (req, res, next) => {
+    try {
+        const payload = {
+            name: req.body.name,
+            budget: req.body.budget
+        }
+
+        await db("accounts").where("id", req.params.id).update(payload)
+        const updatedAccount = await db("accounts").where("id", req.params.id).first()
+
+        res.json(updatedAccount)
+    }catch(err) {
+        next(err)
+    }
+})
+
+router.delete('/:id', async (req, res, next) => {
+    try {
+        await db("accounts").where("id", req.params.id).del()
+        res.status(204).end()
+    } catch(err) {
+        next(err)
+    }
+})
+
+module.exports = router
